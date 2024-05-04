25c88d892d9cd1206b6697b957d6a0a0b2013b01
Harrisistaken <132925337+0xHarrix@users.noreply.github.com>
2024-05-05 00:50:02 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <div className='flex'>
-        <img src='orbs.png' className='orb'></img>
-        <div className='form'>
-          <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" required />
+      <div className="flex-container">
+      <img src='orbs.png' className="orb" alt="Orbs" />
+        <div className="form-container">
+          <div className="form">
+            <h2>Login</h2>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" placeholder="Enter your email" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" placeholder="Enter your password" required />
+            </div>
+            <button type="submit">Sign In</button>
+            <div className="separator">or</div>
+            <button className="google-signin-btn">Sign In with Google</button>
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" required />
-          </div>
-          <button type="submit">Sign In</button>
         </div>
       </div>
     </div>
