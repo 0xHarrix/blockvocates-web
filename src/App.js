378a4b3675bf430d4065c3ae67ab4bb2483ed439
Harrisistaken <132925337+0xHarrix@users.noreply.github.com>
2024-05-05 00:34:05 +0530

    .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div><img src='orbs.png' className='orb'></img></div>
+      <div className='flex'>
+        <img src='orbs.png' className='orb'></img>
+        <div className='form'>
+          <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit">Sign In</button>
+        </div>
+      </div>
     </div>
   );
 }
