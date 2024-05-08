1ae494f1df806543f498d3e7502e847503cc50d3
KumaraKabilan <kumarakabilan2004@gmail.com>
2024-05-08 22:53:21 +0530

    home

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-import { Box } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 
 const Home = () => {
   return (
     <div>
       <NavBar />
-      <Box padding={'200px'}>
-    
+      <Box padding={'50px'}>
+        <Heading as="h1" size="xl" color= '#FFF' textAlign="center">
+          Pick your <span style={{ color: '#00BAE2' }}>Blockchain</span> Journey,
+        </Heading>
+        <Heading as="h1" color= '#FFF' size="xl" textAlign="center">
+          Enter the world of <span style={{ color: '#00BAE2' }}>Web3</span>
+        </Heading>
       </Box>      
     </div>
   );
